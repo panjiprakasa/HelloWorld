5d70cf19342480a72b652b8ce20b72a5da17554d
apamdata <apamdata@apamdatas-MacBook-Pro.local>
2013-06-12 14:00:53 +0700

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by apamdata on 6/12/13.
+//  Copyright (c) 2013 apamdata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by apamdata on 6/12/13.
+//  Copyright (c) 2013 apamdata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
